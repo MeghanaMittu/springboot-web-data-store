2248c769055e450d54806396e0eab55222a7b92b
Meghana <meghanakanneganti@gmail.com>
2023-04-27 13:35:50 -0700

    Update and delete user

--- src/main/java/com/webdatastore/springbootwebdatastore/controller/UserController.java
+++ src/main/java/com/webdatastore/springbootwebdatastore/controller/UserController.java
@@ -4,6 +4,7 @@ import com.webdatastore.springbootwebdatastore.exception.ResourceNotFoundExcepti
 import com.webdatastore.springbootwebdatastore.model.User;
 import com.webdatastore.springbootwebdatastore.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,17 +29,39 @@ public class UserController {
     return userRepository.save(user);
     }
 
-    //build get user by REST API
-    @GetMapping("{userName}")
-    public ResponseEntity<User> getUserByName(@PathVariable String userName) {
-        User user = userRepository.findByUserName(userName);
+    //build get user with email by REST API
+    @GetMapping("{email}")
+    public ResponseEntity<User> getUserByName(@PathVariable String email) {
+        User user = userRepository.findByEmail(email);
                 //orElseThrow(() -> new ResourceNotFoundException("User not exist with:" +userName));
                 //not throwing exception here needs to be fixed
         return ResponseEntity.ok(user);
     }
 
     //build update User REST API
-    public ResponseEntity<User> updateUser(String userName, User userDetails) {
-        User updateUser = userRepository.findByUserName(userName)
+    @PutMapping("{email}")
+    public ResponseEntity<User> updateUser(@PathVariable String email, @RequestBody User userDetails) {
+        User updateUserDetails = userRepository.findByEmail(email);
+        //.orElseThrow(() -> new ResourceNotFoundException("User doesn't exist with " +email));
+
+        updateUserDetails.setUserName(userDetails.getUserName());
+        updateUserDetails.setPhoneNumber(userDetails.getPhoneNumber());
+        updateUserDetails.setPassword(userDetails.getPassword());
+
+        userRepository.save(updateUserDetails);
+
+        return ResponseEntity.ok(updateUserDetails);
+    }
+
+    //build delete User REST API
+    @DeleteMapping("{email}")
+    public ResponseEntity<User> delete(@PathVariable String email) {
+
+        User user = userRepository.findByEmail(email);
+        //.orElseThrow(() -> new ResourceNotFoundException("User doesn't exist with " +email));
+
+        userRepository.delete(user);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/webdatastore/springbootwebdatastore/repository/UserRepository.java
+++ src/main/java/com/webdatastore/springbootwebdatastore/repository/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUserName(String userName);
+    User findByEmail(String email);
     //all crud database methods
 }
